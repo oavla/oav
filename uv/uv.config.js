49a672a9110dadb9768f5ab3a5a11f9cc61ea662
Tewaa <123607334+TewaaCao@users.noreply.github.com>
2024-10-05 14:59:44 -0500

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/math/',
+    bare: 'https://any-mobile.any64.de/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
