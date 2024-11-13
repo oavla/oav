d1a4b28517f96cfd7a2cf725d0bb65c21a65efa1
Lavy <123607334+oavla@users.noreply.github.com>
2024-11-12 18:31:25 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/math/',
-    bare: 'https://oavla.any64.de/bear/',
+    bare: 'https://fee.limana.rs/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
